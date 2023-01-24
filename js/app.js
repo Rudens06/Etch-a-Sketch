4674764e95f45d60fafc11b824162f9f8fde3f3a
Reinis Rudens <reinis6000@gmail.com>
2023-01-25 00:04:45 +0200

    transfer side creation to js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", function(){
     var container = document.getElementById("container");
     var side = 16;
 
-    for (i=0; i<side*side; i++) {
+    for (let i=0; i<side*side; i++) {
         let div = document.createElement("div");
         div.classList.add("square-div");
+        div.style.height = `calc(100vh/${side})`
+        div.style.width = `calc(100vh/${side})`
         container.appendChild(div);
     }
 });
+
+    
+
 
