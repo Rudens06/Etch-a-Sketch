81e146ed7ad06d409053869b0cbd3f0dec090416
Reinis Rudens <reinis6000@gmail.com>
2023-01-25 15:34:27 +0200

    add color picker

--- js/app.js
+++ js/app.js
@@ -19,6 +19,12 @@ submitBtn.addEventListener("click", function(){
     }
 });
 
+var blackInput = document.getElementById("black");
+var redInput = document.getElementById("red");
+var greenInput = document.getElementById("green");
+var blueInput = document.getElementById("blue");
+var randomInput = document.getElementById("random");
+
 function makeGrid(side){
     for (let i=0; i<side*side; i++) {
         let div = document.createElement("div");
@@ -26,10 +32,42 @@ function makeGrid(side){
         div.setAttribute("id", `${i}`);
         div.style.height = `calc(100vh/${side})`
         div.style.width = `calc(100vh/${side})`
+        blackInput.checked = false;
+        redInput.checked = false;
+        greenInput.checked = false;
+        blueInput.checked = false;
+        randomInput.checked = false;
         div.addEventListener("mouseover", function(){
-            let RValue = generateRandom(255);
-            let GValue = generateRandom(255);
-            let BValue = generateRandom(255);
+            let RValue;
+            let GValue;
+            let BValue;
+            
+            if (blackInput.checked){
+                RValue = 32;
+                GValue = 33;
+                BValue = 35;
+            }
+            else if (redInput.checked){
+                RValue = 251;
+                GValue = 92;
+                BValue = 92;
+            }
+            else if (greenInput.checked){
+                RValue = 121;
+                GValue = 190;
+                BValue = 32;
+            }
+            else if (blueInput.checked){
+                RValue = 59;
+                GValue = 125;
+                BValue = 216;
+            }
+            else if (randomInput.checked) {
+                RValue = generateRandom(255);
+                GValue = generateRandom(255);
+                BValue = generateRandom(255);
+            }
+
             if (!this.getAttribute("style").includes("background-color")) {
                 this.style.backgroundColor = `rgb(${RValue}, ${GValue}, ${BValue})`;
             }
